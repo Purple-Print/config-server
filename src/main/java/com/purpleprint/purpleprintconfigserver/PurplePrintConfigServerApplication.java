c2c99765c351974b4b3534a62ea0b49ebcb84b41
hyunjungjeon <sksznddl574@gmail.com>
2023-04-07 20:32:10 +0900

    feat : config server 코드 업로드, 설정 파일 주소 제거, private key 제거

---
+++ src/main/java/com/purpleprint/purpleprintconfigserver/PurplePrintConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.purpleprint.purpleprintconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class PurplePrintConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurplePrintConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/purpleprint/purpleprintconfigserver/PurplePrintConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purpleprint.purpleprintconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurplePrintConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
